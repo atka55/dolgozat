4765e0eee321f6111b5c0a476ef07c9ac2fea3c2
atka55 <84836362+atka55@users.noreply.github.com>
2023-09-27 11:23:49 +0200

    folytataskov

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
+
+var tomb = [];
 function Tarol()
 {
-    
+    var obj = ( 
+    document.getElementById("rendszam").value,
+    document.getElementById("uzema").value,
+    document.getElementById("elsot").checked
+    /*document.getElementById("3").checked,
+    document.getElementById("5").checked */
+    )
+    tomb.push(obj);
+
 }
