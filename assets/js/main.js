f6f228a10babca246b0603eb53ecd0bbc4147472
atka55 <84836362+atka55@users.noreply.github.com>
2023-09-27 13:47:41 +0200

    nemsokakesz

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,49 @@
 var tomb = [];
 function Tarol()
 {
-    var obj = ( 
-    document.getElementById("rendszam").value,
-    document.getElementById("uzema").value,
-    document.getElementById("elsot").checked
-    /*document.getElementById("3").checked,
-    document.getElementById("5").checked */
-    )
+    var obj = { 
+    rendsz: document.getElementById("rendszam").value,
+    uzema: document.getElementById("uzema").value,
+    elsot: document.getElementById("elsot").checked,
+    3: document.getElementById("3").checked,
+    5: document.getElementById("5").checked 
+    }
     tomb.push(obj);
 
+}
+
+function Statisztika()
+{
+    document.getElementById("tablazat").styledisplay = "block";
+    var ben = 0;
+    var diz = 0;
+    var elekt = 0;
+    var elsotu = 0;
+    var regi = 0;
+    var uj = 0;
+    var arany = 0;
+    var a3 = 0;
+    var a5 = 0;
+
+    for( let i = 0; i < tomb.length;i++)
+    {
+        if(tomb[i].uzema == "benzin") 
+        {
+            ben++
+        }
+        else if(tomb[i].uzema == "dizel") 
+        {
+            diz++
+        }
+        else
+        {
+            elekt++;
+        }
+        document.getElementById("benzinsz").innerHTML = ben;
+        document.getElementById("dizelsz").innerHTML = diz;
+        document.getElementById("elektsz").innerHTML = elekt;
+
+        
+        
+    }
 }
